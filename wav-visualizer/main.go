e81121c9f32bb9bae6b4e6b40c007720cd1fe98b
broom <broom@dev>
2024-09-28 16:11:11 +0300

    chore segment repository for multi-project

--- wav-visualizer/main.go
+++ wav-visualizer/main.go
@@ -56,7 +56,7 @@ func main() {
 					break
 				}
 
-				if point.Y > 1 || point.Y < -1 { // Example of outlier detection
+ b				if point.Y > 1 || point.Y < -1 { // Example of outlier detection
 					fmt.Printf("Warning: Amplitude value out of range for Channel %d: %.2f\n", channelId, point.Y)
 				}
 
