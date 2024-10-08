3544ddf8cd101db13ba96457c50a09965728acb3
broom <broom@dev>
2024-10-09 02:26:17 +0300

    feat: add chartOptions struct update to support markers

--- pcm_energy_visualizer/chart/chart.go
+++ pcm_energy_visualizer/chart/chart.go
@@ -8,34 +8,60 @@ import (
 	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
-func AudioLineChart(title string, channeledPoints [][]audio.AudioInputPoint) *charts.Line {
+type ChartOptions struct {
+	Title           string
+	ChanneledPoints [][]audio.AudioInputPoint
+	Segments        [][]audio.AudioSegment
+}
+
+func (options *ChartOptions) CreateAudioLineChart() *charts.Line {
 	line := charts.NewLine()
+
 	line.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title: title,
-		}),
-		charts.WithInitializationOpts(opts.Initialization{
-			Width: "1920px",
-		}),
+		charts.WithTitleOpts(opts.Title{Title: options.Title}),
+		charts.WithInitializationOpts(opts.Initialization{Width: "1920px"}),
 	)
 
-	for channelIndex, points := range channeledPoints {
+	for channelIndex, points := range options.ChanneledPoints {
 		xPoints := make([]float64, len(points))
 		convertedPoints := make([]opts.LineData, len(points))
+		channelName := fmt.Sprintf("channel %d", channelIndex)
 
 		for i, point := range points {
 			xPoints[i] = point.X
 			convertedPoints[i] = opts.LineData{Value: point.Y}
 		}
 
-		channelName := fmt.Sprintf("channel%d", channelIndex)
+		line.SetXAxis(xPoints)
+		line.AddSeries(channelName, convertedPoints)
+		line.SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: opts.Bool(true)}))
 
-		line.SetXAxis(xPoints).AddSeries(channelName, convertedPoints).
-			SetSeriesOptions(charts.WithLineChartOpts(
-				opts.LineChart{
-					// Smooth: opts.Bool(true),
-				}),
+		if len(options.Segments) == 0 {
+			continue
+		}
+
+		channelSegments := options.Segments[channelIndex]
+
+		for segmentIndex, segment := range channelSegments {
+			segmentName := fmt.Sprintf("segment %d", segmentIndex)
+
+			// fmt.Printf("segment %d %f %f \n", segmentIndex, segment.Start, segment.End)
+
+			markLineStart := opts.MarkLineNameXAxisItem{
+				Name:  segmentName + " (start)",
+				XAxis: segment.StartSampleIndex,
+			}
+
+			markLineEnd := opts.MarkLineNameXAxisItem{
+				Name:  segmentName + " (end)",
+				XAxis: segment.EndSampleIndex,
+			}
+
+			line.SetSeriesOptions(
+				charts.WithMarkLineNameXAxisItemOpts(markLineStart),
+				charts.WithMarkLineNameXAxisItemOpts(markLineEnd),
 			)
+		}
 
 	}
 
